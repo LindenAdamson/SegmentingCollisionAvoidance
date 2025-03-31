cfd79e04f5fdaef840789b43ef660597447f9ce7
LindenAdamson <lindena@bu.edu>
2025-03-31 03:35:34 -0400

    trajectory sampling, oakd processing progress, better timer and config

--- scripts/SCA_classes.py
+++ scripts/SCA_classes.py
@@ -13,26 +13,35 @@ from numba import njit, prange
 from timeit import default_timer as timer
 from PIL import Image
 
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        Debug_Timer.start(func.__name__)
+        result = func(*args, **kwargs)
+        Debug_Timer.stop(func.__name__)
+        return result
+    return wrapper
+
 class Segmentation_Collision_Avoidance:
-    def __init__(self, config, dt):
-        self.config = config
-        self.dt = dt
-        self.predictor = Predictor(config, self.dt)
+    def __init__(self, config):
+        Config.load(config)
+        self.predictor = Predictor()
     
+    @timeit
     def add_CARLA_image_file(self, imgName):
-        self.dt.start("open_img")
+        Debug_Timer.start("open_img")
         rgbImg = Image.open("../data/rgb" + imgName + ".jpeg")
         depthImg = Image.fromarray(np.loadtxt("../data/depth" + imgName + ".out", delimiter=","))
-        self.dt.stop("open_img")
+        Debug_Timer.stop("open_img")
         self.resize_images(rgbImg, depthImg)
         
+    @timeit
     def add_OAKD_image_file(self, imgName): # TODO: better match rgb and stereo w/ FOV and image sizes in account
-        self.dt.start("open_img")
+        Debug_Timer.start("open_img")
         rgbImg = Image.open("../oakd_data/test/rgb" + imgName + ".png")
         depthImg = np.asarray(Image.open("../oakd_data/test/depth" + imgName + ".png"))[:,:,0].astype(float)
         depthImg = -5.417 * depthImg / 100 + 9.125 # estimation
         depthImg = Image.fromarray(depthImg)
-        self.dt.stop("open_img")
+        Debug_Timer.stop("open_img")
         self.resize_images(rgbImg, depthImg)
 
     def resize_images(self, rgbImg, depthImg):
@@ -41,48 +50,45 @@ class Segmentation_Collision_Avoidance:
         depthImg = np.asarray(depthImg)
         rgb_shape_h, rgb_shape_v = rgbImg.shape[0:2]
         depth_shape_h, depth_shape_v = depthImg.shape
-        c_fov_h, c_fov_v = self.config["color_fov"]
-        d_fov_h, d_fov_v = self.config["depth_fov"]
-        c_h_l = int(rgb_shape_h * (1 - c_fov_h / max(c_fov_h, d_fov_h)) / 2)
-        c_v_l = int(rgb_shape_v * (1 - c_fov_v / max(c_fov_v, d_fov_v)) / 2)
-        d_h_l = int(depth_shape_h * (1 - d_fov_h / max(c_fov_h, d_fov_h)) / 2)
-        d_v_l = int(depth_shape_v * (1 - d_fov_v / max(c_fov_v, d_fov_v)) / 2)
-        if(c_h_l > 0):
-            rgbImg = rgbImg[c_h_l:-c_h_l,:,:]
-        if(c_v_l > 0):
-            rgbImg = rgbImg[:,c_v_l:-c_v_l,:]
-        if(d_h_l > 0):
-            depthImg = depthImg[d_h_l:-d_h_l,:]
-        if(d_v_l > 0):
-            depthImg = depthImg[:,d_v_l:-d_v_l]
+        rgb_fov_h, rgb_fov_v = Config.get("color_fov")
+        depth_fov_h, depth_fov_v = Config.get("depth_fov")
+        rgb_horiz_diff = int(rgb_shape_h * (1 - rgb_fov_h / max(rgb_fov_h, depth_fov_h)) / 2)
+        rgb_vert_diff = int(rgb_shape_v * (1 - rgb_fov_v / max(rgb_fov_v, depth_fov_v)) / 2)
+        depth_horiz_diff = int(depth_shape_h * (1 - depth_fov_h / max(rgb_fov_h, depth_fov_h)) / 2)
+        depth_vert_diff = int(depth_shape_v * (1 - depth_fov_v / max(rgb_fov_v, depth_fov_v)) / 2)
+        if(rgb_horiz_diff > 0):
+            rgbImg = rgbImg[rgb_horiz_diff:-rgb_horiz_diff,:,:]
+        if(rgb_vert_diff > 0):
+            rgbImg = rgbImg[:,rgb_vert_diff:-rgb_vert_diff,:]
+        if(depth_horiz_diff > 0):
+            depthImg = depthImg[depth_horiz_diff:-depth_horiz_diff,:]
+        if(depth_vert_diff > 0):
+            depthImg = depthImg[:,depth_vert_diff:-depth_vert_diff]
         # to same size
-        h, v = self.config["dimensions"]
+        h, v = Config.get("dimensions")
         rgbImg = np.asarray(Image.fromarray(rgbImg).resize((v, h)))
         depthImg = np.asarray(Image.fromarray(depthImg).resize((v, h)))
         self.predictor.receive_img(rgbImg, depthImg)
 
 class Predictor:
-    def __init__(self, config, dt):
-        self.config = config
-        self.dt = dt
-        self.window = Window(config, self.dt)
+    def __init__(self):
+        self.window = Window()
 
     def receive_img(self, rgbImg, depthImg):
         self.window.receive_img(rgbImg, depthImg)
 
 class Window:
-    def __init__(self, config, dt):
-        self.config = config
-        self.dt = dt
-        self.tracking_imgs = self.config["tracking_imgs"]
+    def __init__(self):
+        self.tracking_imgs = Config.get("tracking_imgs")
         self.model = FastSAM("FastSAM-s.pt")
+        # self.model.to('cuda') 2080 has drivers that are too old lol
         self.frames = []
 
     def receive_img(self, rgbImg, depthImg): # TODO: reshape imgs to same size
-        self.dt.start("fastSAM")
+        Debug_Timer.start("fastSAM")
         results = self.model.track(rgbImg, persist=True)[0]
-        self.dt.stop("fastSAM")
-        frame = Frame(self.config, rgbImg, depthImg, results, self.dt)
+        Debug_Timer.stop("fastSAM")
+        frame = Frame(rgbImg, depthImg, results)
         if len(self.frames) == self.tracking_imgs:
             self.frames.pop(0)
         self.frames.append(frame)
@@ -96,7 +102,7 @@ class Window:
             for object in frame.objects:
                 if object.in_scope:
                     if object.id in d:
-                        d[object.id] = d[object.id] + 1
+                        d[object.id] += 1
                     else:
                         d[object.id] = 1
         for i_frames, frame in enumerate(self.frames):
@@ -108,19 +114,15 @@ class Window:
         return objects_in_scope
 
 class Frame:
-    def __init__(self, config, rgbImg, depthImg, results, dt):
-        self.config = config
-        self.dt = dt
+    def __init__(self, rgbImg, depthImg, results):
         self.rgbImg = rgbImg
         self.depthImg = depthImg
-        self.clean_images()
+        # self.clean_images()
         self.results = results
-        self.rows, self.cols = self.config["dimensions"]
+        self.rows, self.cols = Config.get("dimensions")
         self.objects = self.populate_objects()
         self.clean_objects()
-        self.dt.start("make_cartesian")
         self.cartImg = self.make_cartesian()
-        self.dt.stop("make_cartesian")
         # TODO filter objects
 
     def clean_images(self):
@@ -133,23 +135,34 @@ class Frame:
         boxes = self.results.boxes.xywh.cpu()
         outlines = self.results.masks.xy
         for idx, (id, box, outline) in enumerate(zip(ids, boxes, outlines)):
-            obj = Object(self.config, id, box, outline, self.dt)
+            obj = Object(id, box, outline)
             objects.append(obj)
         return objects
     
+    @timeit
     def clean_objects(self):
-        img_size = self.config["dimensions"][0] * self.config["dimensions"][1]
-        min_area = self.config["min_object_area"]
+        img_size = Config.get("dimensions")[0] * Config.get("dimensions")[1]
+        min_area = Config.get("min_object_area")
+        sky_bright_percent = Config.get("sky_brightness_percent")
+        max_percent_sky_overlap = Config.get("max_percent_sky_overlap")
+        rgb_sum = np.sum(self.rgbImg, axis=2)
+        max_bright = np.max(rgb_sum)
+        bright_cutoff = max_bright * (1 - sky_bright_percent)
+        sky = np.where(rgb_sum > bright_cutoff, 1, 0)
         for obj in self.objects:
-            if float(obj.area) / float(img_size) < min_area:
+            if float(obj.area) / float(img_size) < min_area: # object area too small to care
+                obj.set_out_of_scope()
+            if np.sum(np.logical_and(obj.segMask, sky)) / obj.area > max_percent_sky_overlap: # object is the sky
                 obj.set_out_of_scope()
 
+    @timeit
     def make_cartesian(self):
         depth_sphe = np.empty([self.rows, self.cols, 3]) # theta = left/right, phi = up/down, rho = distance        
         depth_cart = np.empty([self.rows, self.cols, 3]) # x = left/right, y = up/down, z = in/out
-        horiz_fov, vert_fov = self.config["fov"]
-        horiz_fov = horiz_fov * math.pi / 180
-        vert_fov = vert_fov * math.pi / 180
+        horiz_fov =  min(Config.get("color_fov")[0], Config.get("depth_fov")[0])
+        vert_fov =  min(Config.get("color_fov")[1], Config.get("depth_fov")[1])
+        horiz_fov *= math.pi / 180
+        vert_fov *= math.pi / 180
         depth_sphe[:,:,0] = np.linspace(-horiz_fov / 2, horiz_fov / 2, num = self.cols)
         depth_sphe[:,:,1] = np.linspace(-vert_fov / 2, vert_fov / 2, num = self.rows)[np.newaxis].T
         depth_sphe[:,:,2] = self.depthImg
@@ -169,9 +182,7 @@ class Frame:
         return depth_cart
 
 class Object:
-    def __init__(self, config, id, box, outline, dt):
-        self.config = config
-        self.dt = dt
+    def __init__(self, id, box, outline):
         self.id = id
         self.box = box
         self.in_scope = True
@@ -182,7 +193,7 @@ class Object:
         if outline.shape[0] < 3:
             self.set_out_of_scope()
             return
-        segMask = np.zeros(self.config["dimensions"])
+        segMask = np.zeros(Config.get("dimensions"))
         polygon = Polygon(outline)
         points = np.array([[[x, y] for x, y in zip(*polygon.boundary.coords.xy)]]).astype(int)
         segMask = cv2.fillPoly(segMask, points, color=1).astype(bool)
@@ -192,41 +203,48 @@ class Object:
         self.in_scope = False
 
 class Debug_Timer:
-    def __init__(self, debug: bool):
-        self.debug = debug
-        self.start_time = {}
-        self.samples = {}
-        self.total = {}
-        self.max = {}
-        self.min = {}
+    start_time = {}
+    samples = {}
+    total = {}
+    maxi = {}
+    mini = {}
 
-    def start(self, topic: str):
-        if not self.debug:
-            return
-        self.start_time[topic] = timer()
+    @classmethod
+    def start(cls, topic: str):
+        cls.start_time[topic] = timer()
 
-    def stop(self, topic: str):
-        if not self.debug:
-            return
-        time = timer() - self.start_time[topic]
-        if topic in self.samples:
-            self.samples[topic] = self.samples[topic] + 1
-            self.total[topic] = self.total[topic] + time
-            self.max[topic] = max(self.max[topic], time)
-            self.min[topic] = min(self.max[topic], time)
+    @classmethod
+    def stop(cls, topic: str):
+        time = timer() - cls.start_time[topic]
+        if topic in cls.samples:
+            cls.samples[topic] += 1
+            cls.total[topic] += time
+            cls.maxi[topic] = max(cls.maxi[topic], time)
+            cls.mini[topic] = min(cls.mini[topic], time)
         else:
-            self.samples[topic] = 1
-            self.total[topic] = time
-            self.max[topic] = time
-            self.min[topic] = time
+            cls.samples[topic] = 1
+            cls.total[topic] = time
+            cls.maxi[topic] = time
+            cls.mini[topic] = time
 
-    def print_all(self):
-        if not self.debug:
-            return
-        for key in self.samples:
+    @classmethod
+    def print_all(cls):
+        for key in cls.samples:
             print(key + ":")
-            print("\tavg = " + str(self.total[key] / self.samples[key])\
-                + ", min = " + str(self.min[key])\
-                + ", max = " + str(self.max[key]))
-            print("\ttotal = " + str(self.total[key])\
-                + ", " + str(self.samples[key]) + " samples")
+            print("\tavg = " + str(cls.total[key] / cls.samples[key])\
+                + ", min = " + str(cls.mini[key])\
+                + ", max = " + str(cls.maxi[key]))
+            print("\ttotal = " + str(cls.total[key])\
+                + ", " + str(cls.samples[key]) + " samples")
+
+class Config:
+    c = None
+
+    @classmethod
+    def load(cls, name):
+        with open(name + ".yaml") as f:
+            cls.c = yaml.load(f, Loader=yaml.FullLoader)
+
+    @classmethod
+    def get(cls, name):
+        return cls.c[name]
